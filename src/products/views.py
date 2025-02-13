85345cb911ca78a54fb5923c01362dc51069b596
S-aurav <yadavsaurav818@gmail.com>
2025-02-13 18:13:05 +0530

    day 3 of django

--- src/products/views.py
+++ src/products/views.py
@@ -34,12 +34,45 @@ def product_create_view(request):
     }
     return render(request, "products/product_create.html", context)
 
-def dynamic_lookup_view(request, my_id):
+def product_update_view(request, id=id):
+    obj = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=obj)
+
+    if form.is_valid():
+        form.save()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'products/product_create.html', context)
+
+def product_list_view(request):
+    queryset = Product.objects.all() # list of objects
+    context = {
+        'object_list': queryset
+    }
+
+    return render(request, 'products/product_list.html', context)
+
+def product_detail_view(request, id):
+    obj = Product.objects.get(id=id)
+
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
+
+    context = {
+        'object': obj
+    }
+    return render(request, "products/product_details.html", context)
+
+def dynamic_lookup_view(request, id):
     # obj = Product.objects.get(id=my_id)
     # obj = get_list_or_404(Product, id=my_id)
 
     try:
-        obj = Product.objects.get(id=my_id)
+        obj = Product.objects.get(id=id)
     except Product.DoesNotExist:
         raise Http404
     
@@ -49,8 +82,8 @@ def dynamic_lookup_view(request, my_id):
 
     return render(request, 'products/dynamic_product.html', context)
 
-def product_delete_view(request, my_id):
-    obj = get_object_or_404(Product, id=my_id)
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
     #POST request
     if request.method == 'POST':
         # confirming delete
@@ -63,23 +96,4 @@ def product_delete_view(request, my_id):
 
     return render(request, 'products/product_delete.html', context)
 
-def product_list_view(request):
-    queryset = Product.objects.all() # list of objects
-    context = {
-        'object_list': queryset
-    }
 
-    return render(request, 'products/product_list.html', context)
-
-def product_detail_view(request):
-    obj = Product.objects.get(id=1)
-
-    # context = {
-    #     'title': obj.title,
-    #     'description': obj.description
-    # }
-
-    context = {
-        'object': obj
-    }
-    return render(request, "products/product_details.html", context)
